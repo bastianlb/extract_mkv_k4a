a49ceb7dbdfb2219c5d65b8c98f569c20bc09003
bastianl <bastian.lennart@gmail.com>
2022-01-15 12:33:04 +0100

    annotation processing scripts

---
+++ scripts/color_annotation_export.py
@@ -0,0 +1,81 @@
+import os
+import logging
+import pandas as pd
+
+from datetime import timedelta
+
+from mkv_extractor import set_log_level
+from mkv_extractor import TimesynchronizerPCPD, ExportConfig, Path as MkvPath
+
+# "/atlasarchive/atlas/03_animal_trials/210824_animal_trial_02/"
+
+# INPUT_DIR = "/data/input"
+INPUT_DIR = "/media/narvis/Elements/03_animal_trials/"
+EXPORT_DIR = "/data/export/"
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename="annotation_export.log",
+                        level=logging.DEBUG)
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    # set a format which is simpler for console use
+    formatter = logging.Formatter('%(name)-6s: %(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    # add the handler to the root logger
+    logging.getLogger('').addHandler(console)
+    logging.info("Color Frame Export")
+    logging.info(os.listdir(INPUT_DIR))
+
+    set_log_level("info")
+
+    annotations = pd.read_csv("../annotations_processed.csv", parse_dates=["Start", "End"],
+                              infer_datetime_format=True)
+
+    for i, trial in annotations.iterrows():
+        recording_dir = trial["Trial"]
+        if recording_dir not in os.listdir(INPUT_DIR):
+            logging.info("Trial not found: " + recording_dir)
+            continue
+
+        phase = trial["Phase"]
+        trial_path = os.path.join(INPUT_DIR, recording_dir, "recordings")
+        export_path = os.path.join(EXPORT_DIR, recording_dir, trial["Phase"])
+        recording_name = "recordings_" + recording_dir[4:6] + recording_dir[2:4] + "_recording_" + trial["filekey"]
+        dir_path = os.path.join(trial_path, recording_name)
+        if "blooper" in trial["Phase"]:
+            continue
+        if os.path.exists(export_path):
+            logging.warning(f"Skipping recording {export_path}, it exists .. continuing")
+            continue
+        if not os.path.exists(dir_path):
+            logging.warning(f"Skipping recording {export_path}, dir path not found.")
+            continue
+
+        start = trial["Start"]
+        end = trial["End"]
+        phase = trial["Phase"]
+
+        if (end - start) > timedelta(minutes=10):
+            # use only the first 10 minutes right now, don't export forever
+            end = start + timedelta(minutes=10)
+
+        export_config = ExportConfig()
+        export_config.export_color = True
+        export_config.timesync = True
+        export_config.skip_frames = 10
+        export_config.start_ts = timedelta(microseconds=start.value // 1000)
+        export_config.end_ts = timedelta(microseconds=end.value // 1000)
+        logging.info("Exporting color images for: ")
+        logging.info(trial)
+
+        timesync = TimesynchronizerPCPD(export_config)
+        files = [os.path.join(dir_path, x) + "/" for x in os.listdir(dir_path)]
+
+        os.makedirs(export_path, exist_ok=True)
+        logging.info(f"Exporting color images for {export_path}")
+        timesync.initialize_feeds(files, MkvPath(export_path))
+
+        timesync.run()
+
+        del timesync
---
+++ scripts/preprocess_annotations.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import piso
+import numpy as np
+from dateutil.parser import isoparse
+import logging
+
+
+def custom_date_parser(date):
+    # we used custom format.. month-day hrs:mins:sec,ms
+    if not isinstance(date, str) or date == "nan":
+        return np.nan
+    try:
+        d, ms = date.split(",")
+    except ValueError as e:
+        logging.info("Invalid date format: ", date)
+        raise e
+    assert(len(ms) == 3)
+    # put into ISO format
+    date = date.replace(" ", "T")
+    date = date.replace(",", ".")
+    try:
+        return isoparse("2021-" + date)
+    except ValueError as e:
+        logging.info(f"Invalid date format: {date}")
+        raise e
+    return date
+
+
+def read_data(fname):
+    return pd.read_csv(
+        fname,
+        usecols=["Trial", "filekey", "Phase", "Start", "End", "notes"],
+        dtype={"Trial": str, "filekey": str, "Phase": str, "Start": str, "End": str, "notes": str},
+        infer_datetime_format=False, date_parser=custom_date_parser,
+        parse_dates=["Start", "End"])
+
+
+def _in(key, df):
+    return not np.all(df["Phase"] == key)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    fname = "../atlas_workflow_annotations_1401_11:48.csv"
+    annotations = read_data(fname)
+
+    for i, trial in enumerate(annotations["Trial"]):
+        trial = trial.split("_")
+        trial[0] = "21" + trial[0][2:] + trial[0][:2]
+        trial.insert(1, "animal")
+        annotations.loc[i, ("Trial")] = "_".join(trial)
+
+    for trial in annotations["Trial"].unique():
+        filt = annotations[annotations["Trial"] == trial]
+        if not (_in("PAO", filt) and _in("TIP", filt)):
+            logging.info("Trial incomplete!")
+            continue
+
+        # find region of overlap
+    for i, trial in annotations.iterrows():
+        if trial["Start"] >= trial["End"]:
+            print("Trial bad formatting", trial)
+
+    # annotations.index = pd.IntervalIndex.from_arrays(annotations["Start"], annotations["End"])
+    # annotations = annotations.drop(columns=["Start", "End"])
+    to_append = []
+
+    for trial in annotations["Trial"].unique():
+        pao = annotations[(annotations["Trial"] == trial) & (annotations["Phase"] == "PAO")]
+        tip = annotations[(annotations["Trial"] == trial) & (annotations["Phase"] == "TIP")]
+        if (len(pao) == 0 or len(tip) == 0):  # noqa
+            logging.info(f"Trial missing annotations: {trial}")
+            continue
+        assert(pao["filekey"].values[0] == tip["filekey"].values[0])
+        pao_i = pd.arrays.IntervalArray([pd.Interval(pd.Timestamp(pao["Start"].values[0]), pd.Timestamp(pao["End"].values[0]))])  # noqa
+        tip_i = pd.arrays.IntervalArray([pd.Interval(pd.Timestamp(tip["Start"].values[0]), pd.Timestamp(tip["End"].values[0]))])  # noqa
+        inter = piso.intersection(pao_i, tip_i)
+        if len(inter) == 0:
+            # no overlap between segments
+            continue
+        to_append.append([trial, pao["filekey"].values[0], "PREP", inter[0].left, inter[0].right, ""])
+        diff_pao = piso.difference(pao_i, tip_i)
+        diff_tip = piso.difference(tip_i, pao_i)
+        # modify phases exclusively
+        # process pao
+        if len(diff_pao) == 1:
+            annotations.loc[pao.index[0], ["Start"]] = diff_pao[0].left
+            annotations.loc[pao.index[0], ["End"]] = diff_pao[0].right
+        elif len(diff_pao) == 0:
+            # phase is contained entirely in overlap
+            annotations.drop(pao.index[0], inplace=True)
+        elif len(diff_pao) == 2:
+            annotations.loc[pao.index[0], ["Start"]] = diff_pao[0].left
+            annotations.loc[pao.index[0], ["End"]] = diff_pao[0].right
+            to_append.append([trial, pao["filekey"].values[0], "PAO2", diff_pao[0].left, diff_pao[0].right, ""])
+        else:
+            print(f"Invalid set difference {diff_pao}")
+            exit(1)
+        # process tip
+        if len(diff_tip) == 1:
+            annotations.loc[tip.index[0], ["Start"]] = diff_tip[0].left
+            annotations.loc[tip.index[0], ["End"]] = diff_tip[0].right
+        elif len(diff_tip) == 0:
+            # phase is contained entirely in overlap
+            annotations.drop(tip.index[0], inplace=True)
+        elif len(diff_tip) == 2:
+            annotations.loc[tip.index[0], ["Start"]] = diff_tip[0].left
+            annotations.loc[tip.index[0], ["End"]] = diff_tip[0].right
+            to_append.append([trial, tip["filekey"].values[0], "TIP2", diff_tip[0].left, diff_tip[0].right, ""])
+        else:
+            print(f"Invalid set difference {diff_tip}")
+            exit(1)
+
+    out = pd.DataFrame(to_append, columns=annotations.columns)
+    annotations = annotations.append(out)
+    annotations.to_csv("../annotations_processed.csv")
