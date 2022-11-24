2ffd61537b39493e037507e90b319f82f44e35b3
Tony Wang <tonywang008@gmail.com>
2022-11-24 09:37:27 +0100

    changed some scripts and formatting :>

--- scripts/color_annotation_export.py
+++ scripts/color_annotation_export.py
@@ -14,13 +14,16 @@ from mkv_extractor import TimesynchronizerPCPD, ExportConfig, Path as MkvPath
 # INPUT_DIR = "/media/narvis/Elements/03_animal_trials/"
 # INPUT_DIR = "/media/narvis/Elements/03_animal_trials/"
 
-INPUT_DIRS = ["/mnt/atlas_1/03_animal_trials/", "/mnt/atlas_2/03_animal_trials/"]
-EXPORT_DIR = "/archive/2605_di_export/"
-
+INPUT_DIRS = [
+    "/mnt/atlas_1/03_animal_trials/", "/mnt/atlas_2/03_animal_trials/",
+    "/mnt/recordings/03_animal_trials/"
+]
+EXPORT_DIR = '/data/tmp'
+# EXPORT_DIR = "/archive/2605_di_export/"
+# EXPORT_DIR = '/home/tonyw/extract_mkv_k4a/tmp_output'
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="annotation_export.log",
-                        level=logging.DEBUG)
+    logging.basicConfig(filename="annotation_export.log", level=logging.DEBUG)
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
     # set a format which is simpler for console use
@@ -32,22 +35,27 @@ if __name__ == "__main__":
     for inp_dir in INPUT_DIRS:
         logging.info(os.listdir(inp_dir))
 
-
     set_log_level("info")
 
-    annotations = pd.read_csv("../annotations_processed.csv", parse_dates=["Start", "End"],
+    annotations = pd.read_csv("../annotations_processed.csv",
+                              parse_dates=["Start", "End"],
                               usecols=["Trial", "filekey", "Phase", "Start", "End"],
-                              dtype={"Trial": str, "filekey": str, "Phase": str, "Start": str,
-                                     "End": str},
+                              dtype={
+                                  "Trial": str,
+                                  "filekey": str,
+                                  "Phase": str,
+                                  "Start": str,
+                                  "End": str
+                              },
                               infer_datetime_format=True)
 
-    for i, trial in annotations.iterrows():
+    # [14,24,36,46,53,62,72,83,93,115,128,138,148,162,172, 191]
+    # 146 and beyond is calibration 22...
+    for i, trial in annotations[0:1].iterrows():
         recording_dir = trial["Trial"]
 
-        # check in which input dir the trial is
-        INPUT_DIR = next((inp_dir for inp_dir in INPUT_DIRS 
-            if os.path.exists(os.path.join(inp_dir, recording_dir))), None)
-
+        INPUT_DIR = next((inp_dir for inp_dir in INPUT_DIRS
+                          if os.path.exists(os.path.join(inp_dir, recording_dir))), None)
 
         if INPUT_DIR is None:
             logging.info("Trial not found: " + recording_dir)
@@ -56,25 +64,30 @@ if __name__ == "__main__":
         phase = trial["Phase"]
         trial_path = os.path.join(INPUT_DIR, recording_dir, "recordings")
         export_path = os.path.join(EXPORT_DIR, recording_dir, trial["Phase"])
-        recording_name = "recordings_" + recording_dir[4:6] + recording_dir[2:4] + "_recording_" + str(trial["filekey"])
+        recording_name = "recordings_" + recording_dir[4:6] + recording_dir[
+            2:4] + "_recording_" + str(trial["filekey"])
         dir_path = os.path.join(trial_path, recording_name)
 
         export_config = ExportConfig()
-        # export_config.export_color = True
+        export_config.export_color = True
         export_config.export_pointcloud = True
         export_config.align_clouds = True
         export_config.timesync = True
 
         if "blooper" in trial["Phase"]:
             continue
-        
+
         if os.path.exists(export_path):
             # check for specific files instead of whether the directory exists
-            if export_config.export_color and len(glob.glob(export_path + '/**/*.jpg', recursive=True)) > 0:
-                logging.warning(f"Skipping recording {export_path}, color images already exist .. continuing")
+            if export_config.export_color and len(
+                    glob.glob(export_path + '/**/*.jpg', recursive=True)) > 0:
+                logging.warning(
+                    f"Skipping recording {export_path}, color images already exist .. continuing")
                 continue
-            if export_config.export_pointcloud and len(glob.glob(export_path + '/**/*.ply', recursive=True)) > 0:
-                logging.warning(f"Skipping recording {export_path}, point clouds already exist .. continuing")
+            if export_config.export_pointcloud and len(
+                    glob.glob(export_path + '/**/*.ply', recursive=True)) > 0:
+                logging.warning(
+                    f"Skipping recording {export_path}, point clouds already exist .. continuing")
                 continue
 
         if not os.path.exists(dir_path):
@@ -83,28 +96,37 @@ if __name__ == "__main__":
 
         start = pd.Timestamp(trial["Start"])
         end = pd.Timestamp(trial["End"])
+        mid = start + (end - start) / 2
         phase = trial["Phase"]
+        new_start = start
+        new_end = end
+        #new_start = mid - timedelta(seconds=50)
 
-        if (end - start) > timedelta(minutes=15):
+        #if new_start < start:
+        #    new_start = start
+
+        #new_end = mid + timedelta(seconds=50)
+        #if new_end > end:
+        #    new_end = end
+
+        if (end - start) > timedelta(seconds=30):
             # use only the first 15 minutes right now, don't export forever
-            end = start + timedelta(minutes=15)
+            end = start + timedelta(seconds=30)
 
-
-        # only export 1FPS
+        # only export 0.5FPS
         export_config.skip_frames = 5
-        export_config.start_ts = timedelta(microseconds=start.value // 1000)
-        export_config.end_ts = timedelta(microseconds=end.value // 1000)
-        logging.info("Exporting color images for: ")
+        export_config.start_ts = timedelta(microseconds=new_start.value // 1000)
+        export_config.end_ts = timedelta(microseconds=new_end.value // 1000)
+        logging.info("Exporting data for: ")
         logging.info(trial)
 
         timesync = TimesynchronizerPCPD(export_config)
         files = [os.path.join(dir_path, x) + "/" for x in os.listdir(dir_path)]
 
         os.makedirs(export_path, exist_ok=True)
-        logging.info(f"Exporting color images for {export_path}")
+        logging.info(f"Exporting data to {export_path}")
         timesync.initialize_feeds(files, MkvPath(export_path))
 
         timesync.run()
 
         del timesync
-
--- scripts/color_export_tony.py
+++ scripts/color_export_tony.py
@@ -1,47 +1,66 @@
 import os
-from datetime import timedelta
+from datetime import timedelta, datetime
+import re
 
 from mkv_extractor import set_log_level
 from mkv_extractor import TimesynchronizerPCPD, ExportConfig, Path as MkvPath
 
-
-INPUT_DIR = "/mnt/atlas_2/"
+DAY = "23"
+MONTH = "11"
+YEAR = "21"
+HOUR = "7"
+MINUTE = "26"
+SECONDS = "30"
+RECORDING = "2"
 
 if __name__ == "__main__":
     print("Color Video export")
-    print(os.listdir(INPUT_DIR))
     export_config = ExportConfig()
     export_config.export_color = True
-    export_config.max_frames_exported = 100000
+    export_config.max_frames_exported = 200
     export_config.skip_frames = 5
     export_config.export_distorted = False
-    export_config.export_pointcloud = True
+    export_config.export_pointcloud = False
     export_config.timesync = True
-    export_config.align_clouds = True
-    export_config.export_rgbd = True
+    export_config.align_clouds = False
+    export_config.export_rgbd = False
 
-    # TODO: start end must be recording dependent...
-    #                        1628650257725706448
-    #export_config.start_ts = timedelta(microseconds=1628651055148343000 // 1000)
-    #export_config.end_ts =   timedelta(microseconds=1628653016064450000 // 1000)
-    #                        1628658895596854977
-
-    export_config.start_ts = timedelta(seconds=1639478820) # Tuesday, December 14, 2021 10:47:00 AM GMT
-    export_config.end_ts = timedelta(seconds=1639479180) # Tuesday, December 14, 2021 10:53:00 AM
-    #export_config.start_ts = timedelta(seconds=1635838893)
+    unix_time = datetime(year=int(f'20{YEAR}'),
+                         month=int(MONTH),
+                         day=int(DAY),
+                         hour=int(HOUR) + 1,
+                         minute=int(MINUTE),
+                         second=int(SECONDS)).timestamp()
+    export_config.start_ts = timedelta(
+        seconds=unix_time)  # 2nd Nov. 2021, 7.40.33 AM
+    export_config.end_ts = export_config.start_ts + timedelta(minutes=2)
 
     set_log_level("info")
 
-    # for base_dir in ["03_animal_trials/211102_animal_trial_08"]:  #, "recordings_2809_recording_5"]:
-    #     print("Exporting color frames for: ", base_dir)
-
     timesync = TimesynchronizerPCPD(export_config)
-    # dir_path = os.path.join(INPUT_DIR, base_dir)
-    dir_path = "/mnt/atlas_2/03_animal_trials/211214_animal_trial_17/recordings/recordings_1412_recording_04/"
+
+    trial_name_regex = f"{YEAR.zfill(2)}{MONTH.zfill(2)}{DAY.zfill(2)}_animal_trial_*"
+
+    directory_atlas_1 = "/mnt/atlas_1/03_animal_trials/"
+    directory_atlas_2 = "/mnt/atlas_2/03_animal_trials/"
+    trials_1 = os.listdir(directory_atlas_1)
+    trials_2 = os.listdir(directory_atlas_2)
+
+    r = re.compile(trial_name_regex)
+    trials_1 = list(filter(r.match, trials_1))
+    trials_2 = list(filter(r.match, trials_2))
+    assert (len(trials_1) == 1 or len(trials_2) == 1)
+    if len(trials_1) == 1:
+        trial_dir = directory_atlas_1 + trials_1[0]
+    else:
+        trial_dir = directory_atlas_2 + trials_2[0]
+
+    dir_path = trial_dir + f"/recordings/recordings_{DAY.zfill(2)}{MONTH.zfill(2)}_recording_{RECORDING.zfill(2)}/"
     files = [os.path.join(dir_path, x) + "/" for x in os.listdir(dir_path)]
     print("Files:", files)
 
-    timesync.initialize_feeds(files, MkvPath("/home/tonyw/extract_mkv_k4a/output/trial_17_recording_04/"))
+    timesync.initialize_feeds(
+        files, MkvPath("/home/tonyw/extract_mkv_k4a/output/test/"))
 
     timesync.run()
 
